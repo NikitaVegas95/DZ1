4f265e79dc8f4df8f86ad53ee65f4e4fb85954bd
Isaev Nikita <nikitavegas95@gmail.com>
2025-05-31 13:32:21 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+
+const users = [{
+    name: 'name1',
+    age: '331',
+    id: 1,
+},
+    {
+        name: 'name2',
+        age: '332',
+        id: 2,
+    },{
+        name: 'name3',
+        age: '333',
+        id: 3,
+    }]
+
+
+function App() {
+
+  return (
+    <>
+        {users.map((user) => (
+            <div key={user.id}>
+                <p>{user.name}</p>
+                <p>{user.age}</p>
+            </div>
+        ))}
+    </>
+  )
+}
+
+export default App
